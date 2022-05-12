cf7a632739a39c0c750dc3ae9d248283b05ea675
gonelastvirus <gonelastvirus@gmail.com>
2022-05-12 14:23:39 +0545

    Updates

---
+++ precache-manifest.956d41ee94ef0b4aebbc21e10b04372c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5208edc16ad6603b651b",
+    "url": "/reactapp/static/css/main.1dbf22ae.chunk.css"
+  },
+  {
+    "revision": "5208edc16ad6603b651b",
+    "url": "/reactapp/static/js/main.5208edc1.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/reactapp/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "229286ead9ca91fcb30e",
+    "url": "/reactapp/static/js/2.229286ea.chunk.js"
+  },
+  {
+    "revision": "e26e5ebc1ef3b811bc57",
+    "url": "/reactapp/static/js/runtime~main.e26e5ebc.js"
+  },
+  {
+    "revision": "dad1fe4d094fbe2593d992bb6fb6f264",
+    "url": "/reactapp/index.html"
+  }
+];
